b573820b74cecea30c7a31257ae440394427fad7
a-harland <asa.harland@e.lloydsbanking.com>
2021-11-08 20:07:23 +0000

    feat: external workflow.

---
+++ envs/tst/backend.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 0.14"
+
+  backend "remote" {
+    organization = "harland"
+
+    workspaces {
+      name = "tf-native-tst"
+    }
+  }
+}
+
+provider "google" {
+  project = "blue-dog-piano"
+}
+
+provider "google-beta" {
+  project = "blue-dog-piano"
+}
+
+locals {
+  project_id = "blue-dog-piano"
+}
--- modules/iam/service_accounts.tf
+++ modules/iam/service_accounts.tf
@@ -2,18 +2,3 @@ resource "google_service_account" "service_account" {
   account_id   = var.service_account_name
   display_name = "Service Account"
 }
-
-resource "google_project_iam_policy" "project" {
-  project     = "blue-dog-piano"
-  policy_data = data.google_iam_policy.admin.policy_data
-}
-
-data "google_iam_policy" "admin" {
-  binding {
-    role = "roles/editor"
-
-    members = [
-      "user:asa.harland@e.lloydsbanking.com",
-    ]
-  }
-}
