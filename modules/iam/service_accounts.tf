84b8bd6388d6a8f11931b2f5175ff995e4690518
Asa Harland <92163244+a-harland@users.noreply.github.com>
2021-11-09 13:36:32 +0000

    Update service_accounts.tf

--- modules/iam/service_accounts.tf
+++ modules/iam/service_accounts.tf
@@ -3,17 +3,17 @@ resource "google_service_account" "service_account" {
   display_name = "Service Account"
 }
 
-resource "google_project_iam_policy" "project" {
-  project     = var.project_id
-  policy_data = data.google_iam_policy.admin.policy_data
-}
+# resource "google_project_iam_policy" "project" {
+#   project     = var.project_id
+#   policy_data = data.google_iam_policy.admin.policy_data
+# }
 
-data "google_iam_policy" "admin" {
-  binding {
-    role = "roles/editor"
+# data "google_iam_policy" "admin" {
+#   binding {
+#     role = "roles/editor"
 
-    members = [
-      "user:asa.harland@lloydsbanking.com",
-    ]
-  }
-}
+#     members = [
+#       "user:asa.harland@lloydsbanking.com",
+#     ]
+#   }
+# }
