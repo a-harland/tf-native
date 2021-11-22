241095ba505d324d8c92a2dae7b2218bbff0ab5d
Asa Harland <92163244+a-harland@users.noreply.github.com>
2021-11-22 15:07:08 +0000

    feat: updated SA name

--- modules/iam/service_accounts.tf
+++ modules/iam/service_accounts.tf
@@ -1,6 +1,6 @@
 resource "google_service_account" "service_account" {
   account_id   = var.service_account_name
-  display_name = "Service Account"
+  display_name = "Service Account Name"
 }
 
 resource "google_project_iam_binding" "project" {
