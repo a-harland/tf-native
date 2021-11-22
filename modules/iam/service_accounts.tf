07dc1007609c3c424f34b6d8a984f8d231fbd9e8
Asa Harland <92163244+a-harland@users.noreply.github.com>
2021-11-22 15:40:17 +0000

    feat: admin

--- modules/iam/service_accounts.tf
+++ modules/iam/service_accounts.tf
@@ -5,7 +5,7 @@ resource "google_service_account" "service_account" {
 
 resource "google_project_iam_binding" "project" {
   project = var.project_id
-  role    = "roles/storage.objectViewer"
+  role    = "roles/storage.admin"
 
   members = [
     "user:asa.harland@e.lloydsbanking.com",
