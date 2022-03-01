d288c230d95483c77bafb8678ee65fb61070492d
a-harland <asa.harland@e.lloydsbanking.com>
2022-03-01 16:25:38 +0000

    Fixing format.

--- envs/tst/main.tf
+++ envs/tst/main.tf
@@ -25,7 +25,7 @@ resource "google_bigquery_dataset" "dataset" {
   description                 = "This is a test description"
   location                    = "europe-west2"
   default_table_expiration_ms = 3600000
-  
+
   labels = {
     owner              = "a-harland"
     troux_id           = "troux"
